c309892fd52c0d2c95ae3700daa709d3935531cc
Tevfik Hacıhamdioğlu <tevfikhoglu@gmail.com>
2022-03-05 18:52:38 +0300

    second push

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Todo } from '../model'
 import { AiFillEdit, AiFillDelete } from 'react-icons/ai'
 import { MdDone } from 'react-icons/md'
@@ -26,10 +26,29 @@ const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
     setTodos(todos.filter(todo => todo.id !== id))
   }
 
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault()
+
+    setTodos(
+      todos.map(todo => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    )
+    setEdit(false)
+  }
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [edit])
+
   return (
-    <form className='todos__single'>
+    <form className='todos__single' onSubmit={e => handleEdit(e, todo.id)}>
       {edit ? (
-        <input />
+        <input
+          value={editTodo}
+          onChange={e => setEditTodo(e.target.value)}
+          className='todos__single--text'
+        />
       ) : todo.isDone ? (
         <s className='todos__single--text'>{todo.todo}</s>
       ) : (
@@ -40,7 +59,7 @@ const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }) => {
         <span
           className='icon'
           onClick={() => {
-            if (!edit && todo.isDone) {
+            if (!edit && !todo.isDone) {
               setEdit(!edit)
             }
           }}>
